d485a3968520b90ade7ac742cc09c114b9f598b6
kachamachkov <kachamachkovboyan@gmail.com>
2023-12-17 12:27:57 +0200

    add event function to remove tasks

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function addTask() {
     }
     inputBox.value = ''; //reset value after task
 }
+
+listContainer.addEventListener(
+    'click',
+    function (e) {
+        if (e.target.tagName === 'LI') {
+            e.target.classList.toggle('checked');
+        } else if (e.target.tagName === 'SPAN') {
+            e.target.parentElement.remove();
+        }
+    },
+    false
+);
